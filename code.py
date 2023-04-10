9e2693bbeed7d94294d9bdc1f42dec2a41a24c31
Sandro Meier <sandro@sandro-meier.ch>
2023-04-10 19:57:25 +0200

    Fix code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@ from digitalio import DigitalInOut, Direction
 led = DigitalInOut(board.LED_GREEN)
 led.direction = Direction.OUTPUT
 
-led.value = True
+led.value = False
 
 while True:
     time.sleep(0.1)
